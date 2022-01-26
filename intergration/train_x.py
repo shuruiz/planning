e3bd8ecb3437e3cbc030f3a1c55244e63d9ee03c
lab1 <zhangzhbd@gmail.com>
2022-01-26 01:34:16 -0500

    modelx update

--- intergration/train_x.py
+++ intergration/train_x.py
@@ -4,7 +4,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 # from model import _build_model, _build_simple_model, _build_simple_model2
 from model_reduced import _build_model, _build_simple_model, _build_simple_model2, _build_reduced_model, _build_reduced_model2
-from core8 import Graph
+from corex import Graph
 
 import os
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   
